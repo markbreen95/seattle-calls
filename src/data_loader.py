dc17497d996649952c901073eb7bcd69ca191fc7
Mark Breen <markbreen15@gmail.com>
2022-09-22 17:20:42 +0200

    adding training code

---
+++ src/data_loader.py
@@ -0,0 +1,20 @@
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+class DataProcessor():
+    def __init__(self, credentials_path, project_id):
+        self.credentials = service_account.Credentials.from_service_account_file(
+            credentials_path
+        )
+        self.project_id = project_id
+        self.client = bigquery.Client(credentials=self.credentials, project=self.project_id)
+
+    def query_db(self, mode):
+        if mode == 'train':
+            with open('train.sql', 'r') as f:
+                query_str = f.read()
+        else:
+            with open('validation.sql', 'r') as f:
+                query_str = f.read()
+        query_job = self.client.query(query_str)
+        return query_job.to_dataframe()
---
+++ train.sql
@@ -0,0 +1,1 @@
+SELECT EXTRACT(YEAR FROM report_date) AS year, EXTRACT(MONTH FROM report_date) AS month, EXTRACT(DAY FROM report_date) AS day, CAST(SUBSTR(report_time, 0, 2) AS INT) AS hour, Longitude, Latitude FROM `niologic-assessment.seattle_calls.calls-processed` WHERE EXTRACT(YEAR FROM report_date) BETWEEN 2019 AND 2021;
